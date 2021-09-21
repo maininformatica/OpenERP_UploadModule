d27df56b506464b08dbd7afc560c1bffbd3e0525
main informàtica <github@main-informatica.com>
2021-09-21 12:33:07 +0200

    Update __manifest__.py

--- openerp_uploadmodule/__manifest__.py
+++ openerp_uploadmodule/__manifest__.py
@@ -12,7 +12,7 @@
     'website': "https://www.main-informatica.com/magazine/openerp/",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Extra Tools',
     'version': '0.1',
